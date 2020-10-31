4579245fae5a6aae3ee3d0d99ffb5f9c088d173d
Ryan Connell <7585908+RyanConnell@users.noreply.github.com>
2020-10-31 01:05:22 +0000

    Install vim-plug

--- setup.sh
+++ setup.sh
@@ -1,6 +1,10 @@
 # Install Oh-My-ZSH
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
+# Install github.com/junegunn/vim-plug
+mkdir -p ~/.vim/autoload/
+wget -O ~/.vim/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 # Backup existing dotfiles if they exist.
 mv $HOME/.zshrc $HOME/.zshrc.old
 mv $HOME/.vimrc $HOME/.vimrc.old
