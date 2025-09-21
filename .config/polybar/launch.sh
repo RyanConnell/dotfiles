2c46743bb2d2994fcaea21ef572cd508bb324382
Ryan Connell <ryanconnell96@gmail.com>
2025-09-21 20:57:29 +0100

    feat(repo): Repo redux
    Reorganising this repository - starting from scratch, farewell old
    dotfiles!

--- .config/polybar/launch.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-killall -q polybar
-polybar top >> /tmp/polybar_top.log 2>&1 &
-polybar bottom >> /tmp/polybar_top.log 2>&1 &
--- install-wm.sh
+++
@@ -1,3 +0,0 @@
-sudo add-apt-repository -y ppa:kgilmer/speed-ricer
-sudo apt update
-sudo apt install -y i3-gaps polybar feh picom rofi libpulse fonts-font-awesome
