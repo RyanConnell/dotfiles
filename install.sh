11f48eb5114e0edde337e5e1f8333081dff007f5
Ryan Connell <ryanconnell96@gmail.com>
2025-09-21 23:37:27 +0100

    feat(installation): Backup existing dotfiles to avoid conflicts

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+PACKAGES=$(ls -d */ | sed 's/\///' | xargs)
+
 # --- Ensure that Stow is installed ---
 if ! command -v stow >/dev/null 2>&1; then
     echo "stow is not installed. Attempting to install..."
@@ -19,7 +21,8 @@ if ! command -v stow >/dev/null 2>&1; then
     fi
 fi
 
-PACKAGES=$(ls -d */ | sed 's/\///' | xargs)
+# --- Create backup directory ---
+BACKUP_DIR="backups"
 
 # --- Run Package Pre-Install Scripts ---
 for pkg in $PACKAGES; do
@@ -29,8 +32,28 @@ for pkg in $PACKAGES; do
 done
 
 # --- Install config/dotfiles for each application ---
+TIMESTAMP=$(date +%Y-%m-%d_%H-%M-%S)
 for pkg in $PACKAGES; do
-	stow -R "$pkg" --ignore '(pre|post).sh'
+    # Attempt to stow
+    if ! stow -R "${pkg}" --ignore '(pre|post).sh'; then
+        echo "Stow for '${pkg}' failed; Attempting to backup files"
+        # Find all files in the package directory, excluding pre/post scripts
+        files=$(find "$pkg" -type f -not -path "*/pre.sh" -not -path "*/post.sh")
+
+        # --- Backup any existing files to avoid loss of data ---
+        for file in ${files#$pkg/}; do
+            target_file="$HOME/$file"
+            if [ -f "$target_file" ] && [ ! -L "$target_file" ]; then
+                backup_file="$BACKUP_DIR/$TIMESTAMP/$file"
+                echo "[${pkg}]: Backing up $target_file to $backup_file"
+                mkdir -p "$(dirname $backup_file)"
+                mv "$target_file" "$backup_file"
+            fi
+        done
+
+        # Attempt to stow again now that the files have been moved.
+        stow -R "$pkg" --ignore '(pre|post).sh'
+    fi
 done
 
 # --- Run Package Post-Install Scripts ---
